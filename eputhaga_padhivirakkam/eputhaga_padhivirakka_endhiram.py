5f6f8a73027c7e0d8d1fa9c7b3b82676f863c8c9
Srinivas <srini.tvmalai11@gmail.com>
2018-08-28 18:16:57 +0530

    Retry for connection reset by peer

--- eputhaga_padhivirakkam/eputhaga_padhivirakka_endhiram.py
+++ eputhaga_padhivirakkam/eputhaga_padhivirakka_endhiram.py
@@ -2,6 +2,7 @@ import logging
 import os
 import re
 
+from telegram import TelegramError
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 import ragasiya_padhivirakkam
@@ -12,6 +13,14 @@ logger = logging.getLogger(__name__)
 
 
 def send_document(asin, status, bot, chat_id):
+    """
+    send document to the user
+    :param asin:
+    :param status:
+    :param bot:
+    :param chat_id:
+    :return:
+    """
     status = ragasiya_padhivirakkam.get_book_from_local(asin, status)
     if 'Error:' in status or not os.path.exists(status):
         bot.send_message(chat_id=chat_id, text='Sorry!!! Could not process download and the error is {}'
@@ -28,36 +37,48 @@ def send_document(asin, status, bot, chat_id):
     finally:
         os.remove(status.strip())
 
+
 def start(bot, update, args):
-    asin = args[0]
-    chat_id = update.message.chat_id
-    if len(asin) != 10:
-        bot.send_message(chat_id=chat_id, text="Please provide proper 10 digit ASIN")
-        return
-    pattern = re.compile('^B[a-zA-Z0-9]+$')
-    gr = pattern.match(asin)
-    if gr is None:
-        bot.send_message(chat_id=chat_id, text="Please provide proper 10 digit ASIN")
-        return
-    bot.send_message(chat_id=chat_id, text="Initiating download. Please relax for some time")
-    status = ragasiya_padhivirakkam.download_book(asin)
-    logger.info(' Status of book downloader: {}---'.format(status))
-    logger.info('Does book Local Path {}: '.format(str(os.path.exists(status))))
-    if 'Error:' in status or not os.path.exists(status):
-        logger.info('Sending err msg back to user')
-        bot.send_message(chat_id=chat_id, text='Sorry!!! Could not process download and the error is {}'.format(status),
-                         timeout=1500)
-    else:
-        logger.info('Replying back to user')
-        try:
-            update.message.reply_text("Book Found!!! Please wait for some more time while your book is on the way",
+    """
+    start listening updates from telegram
+    :param bot:
+    :param update:
+    :param args:
+    :return:
+    """
+    try:
+        asin = args[0]
+        chat_id = update.message.chat_id
+        if len(asin) != 10:
+            bot.send_message(chat_id=chat_id, text="Please provide proper 10 digit book id")
+            return
+        pattern = re.compile('^B[a-zA-Z0-9]+$')
+        gr = pattern.match(asin)
+        if gr is None:
+            bot.send_message(chat_id=chat_id, text="Please provide proper 10 digit book id")
+            return
+        bot.send_message(chat_id=chat_id, text="Initiating download. Please relax for some time")
+        status = ragasiya_padhivirakkam.download_book(asin)
+        logger.info(' Status of book downloader: {}---'.format(status))
+        logger.info('Does book Local Path {}: '.format(str(os.path.exists(status))))
+        if 'Error:' in status or not os.path.exists(status):
+            logger.info('Sending err msg back to user')
+            bot.send_message(chat_id=chat_id, text='Sorry!!! Could not process download and the error is {}'.
+                             format(status), timeout=1500)
+        else:
+            logger.info('Replying back to user')
+            try:
+                update.message.reply_text("Book Found!!! Please wait for some more time while your book is on the way",
                                     timeout=1500)
-            send_document(asin, status, bot, chat_id)
-        except Exception as e:
-            logger.info(e.message)
-            bot.send_message(chat_id=chat_id, text='Book Found!!! Please wait for some more time'
-                                                   ' while your book is on the way')
-            send_document(asin, status, bot, chat_id)
+                send_document(asin, status, bot, chat_id)
+            except Exception as e:
+                logger.info(e.message)
+                bot.send_message(chat_id=chat_id, text='Book Found!!! Please wait for some more time'
+                                                    ' while your book is on the way')
+                send_document(asin, status, bot, chat_id)
+    except TelegramError as e:
+        logger.info("TELEGRAMException " + e.message)
+        start(bot, update, args)
 
 
 def error(bot, update, error):
@@ -68,7 +89,7 @@ def error(bot, update, error):
 def main():
     updater = Updater(token='668051538:AAEIRiZdSX3qXCjCE2xLRr3mYTQ4rUG0uno')
     dispatcher = updater.dispatcher
-    start_handler = CommandHandler('asin', start, pass_args=True)
+    start_handler = CommandHandler('bid', start, pass_args=True)
     dispatcher.add_handler(start_handler)
     # log all errors
     dispatcher.add_error_handler(error)
